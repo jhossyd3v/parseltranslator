51749f18638b0ce289e53b627ad02db84a499b56
Jhossymar Contreras <chrisj_contreras@outlook.com>
2020-06-23 23:35:22 -0600

    Traduccion Parsel a Humano, version alpha

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
 window.onload = function () {
     let boton_traducir = document.getElementById('boton_traducir');
+    let boton_cambiar = document.getElementById('boton_cambiar');
     boton_traducir.onclick = traducir_click;
+    boton_cambiar.onclick = cambio_click;
 };
 
 function traducir_click(event) {
-    let ta_normal = document.getElementById('ta_normal');
-    let ta_parsel = document.getElementById('ta_parsel');
+    let ta_from = document.getElementById('ta_from');
+    let ta_to = document.getElementById('ta_to');
 
-    let texto_normal = ta_normal.value;
-    let texto_parsel = parselTranslator.traducir_a_parsel(texto_normal);
-    ta_parsel.value = texto_parsel;
+    let data_traduccion = boton_cambiar.getAttribute('data-traduccion');
+    let texto_from = ta_from.value;
+    let texto_to = '';
+    if (data_traduccion == 'parsel_humano') {
+        texto_to = parselTranslator.traducir_a_parsel(texto_from);
+    } else {
+        texto_to = parselTranslator.traducir_parsel_a_humano(texto_from);
+    }
+    ta_to.value = texto_to;
+}
+
+function cambio_click(event) {
+    let data_traduccion = boton_cambiar.getAttribute('data-traduccion');
+    let label_from = document.querySelector('label[for="ta_from"]');
+    let label_to = document.querySelector('label[for="ta_to"]');
+    let ta_from = document.getElementById('ta_from');
+    let ta_to = document.getElementById('ta_to');
+    if (data_traduccion == 'parsel_humano') {
+        boton_cambiar.setAttribute('data-traduccion', 'humano_parsel');
+        label_from.textContent = 'Humano';
+        label_to.textContent = 'Parsel';
+    } else {
+        boton_cambiar.setAttribute('data-traduccion', 'parsel_humano');
+        label_from.textContent = 'Parsel';
+        label_to.textContent = 'Humano';
+    }
+    ta_from.value = '';
+    ta_to.value = '';
 }
--- js/parselTranslator.js
+++ js/parselTranslator.js
@@ -1,34 +1,36 @@
 (function abecedario() {
     let abecedario = {
-        a: 'esh',
-        b: 'ch',
-        c: 'eish',
-        d: 'shi',
-        e: 'ash',
-        f: 'asha',
-        g: 'ei',
-        h: 'shis',
-        i: 'osh',
-        j: 'xim',
-        k: 'ss',
-        l: 'suh',
-        m: 'xan',
-        n: 'sh',
-        o: 'ush',
-        p: 'cah',
-        q: 'xii',
-        r: 'in',
-        s: 'shs',
-        t: 'cass',
-        u: 'ish',
-        v: 'aus',
-        w: 'xi',
-        x: 'shh',
-        y: 'sss',
-        z: 'xiy'
+        a: "esh",
+        b: "ch",
+        c: "eish",
+        d: "shi",
+        e: "ash",
+        f: "asha",
+        g: "ei",
+        h: "shis",
+        i: "osh",
+        j: "xim",
+        k: "ss",
+        l: "suh",
+        m: "xan",
+        n: "sh",
+        o: "ush",
+        p: "cah",
+        q: "xii",
+        r: "in",
+        s: "shs",
+        t: "cass",
+        u: "ish",
+        v: "aus",
+        w: "xi",
+        x: "shh",
+        y: "sss",
+        z: "xiy",
     };
 
-    let traducir_caracter_a_parsel = function (caracter = '') {
+    let en_parsel = '';
+
+    let traducir_caracter_a_parsel = function (caracter = "") {
         caracter = caracter.toLowerCase();
         let nuevo_caracter = abecedario[caracter];
         if (nuevo_caracter == undefined) {
@@ -36,37 +38,94 @@
         }
 
         return nuevo_caracter;
-    }
+    };
 
-    let traducir_texto_a_parsel = function (texto = '') {
-        let caracteres = texto.split('');
-        let nueva_palabra = '';
+    let traducir_texto_a_parsel = function (texto = "") {
+        let caracteres = texto.split("");
+        let nueva_palabra = "";
 
-        caracteres.forEach(caracter => {
+        caracteres.forEach((caracter) => {
             nueva_palabra += traducir_caracter_a_parsel(caracter);
-        })
+        });
 
         return nueva_palabra;
-    }
+    };
 
     let render_abecedario = function () {
         let abecedario_keys = Object.keys(abecedario);
-        let htmlContent = '';
+        let htmlContent = "";
 
-        abecedario_keys.forEach(letra => {
+        abecedario_keys.forEach((letra) => {
             htmlContent += `<div class="abc_item">
                                 <span class="abc_item--letra"> ${letra} = </span>
                                 <span class="abc_item--parsel">${abecedario[letra]}</span>
                             </div>`;
-        })
+        });
 
         return htmlContent;
-    }
+    };
 
-    if (!window.hasOwnProperty('parselTranslator')) {
+    let traducir_parsel_a_humano = function (texto = "") {
+        let palabras = texto.split(" ");
+        let traduccion = '';
+
+        palabras.forEach((palabra) => {
+            traduccion = traduccion + analizar_palabra_parsel(palabra) + " ";
+        });
+
+        return traduccion.trim();
+    };
+
+    let analizar_palabra_parsel = function (texto = "", inicio = 0) {
+        if (inicio == 0) {
+            en_parsel = '';
+        }
+
+        let porcion = texto.substr(inicio, 4);
+        let coincidencia = buscar_coincidencia_parsel(porcion);
+
+        if (inicio > texto.length) {
+            return en_parsel;
+        } else {
+            en_parsel = en_parsel + coincidencia.coincidencia;
+            inicio += coincidencia.size;
+            return analizar_palabra_parsel(texto, inicio);
+        }
+    };
+
+    let buscar_coincidencia_parsel = function (texto = "", size = 4) {
+        let respuesta = {
+            coincidencia: texto,
+            size: size
+        };
+
+        let porcion = texto.substr(0, size);
+
+        let abecedario_keys = Object.keys(abecedario);
+        let filtrado = abecedario_keys.filter(letra => {
+            return abecedario[letra] == porcion;
+        })
+
+        if (filtrado.length == 0 && size > 2) {
+            let new_size = size - 1;
+            return buscar_coincidencia_parsel(texto, new_size);
+        } else {
+            if (size < 2) {
+                return respuesta;
+            } else {
+                if (filtrado.length > 0) {
+                    respuesta.coincidencia = filtrado[0];
+                }
+                return respuesta;
+            }
+        }
+    };
+
+    if (!window.hasOwnProperty("parselTranslator")) {
         window.parselTranslator = {
             traducir_a_parsel: traducir_texto_a_parsel,
-            render_abecedario: render_abecedario
+            render_abecedario: render_abecedario,
+            traducir_parsel_a_humano: traducir_parsel_a_humano
         };
     }
-})()
+})();
